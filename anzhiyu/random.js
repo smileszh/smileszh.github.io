99d48c705d178ad38aad4703c14f5b559beea536
smileszh <smileszh@126.com>
2023-06-12 09:52:56 +0800

    Site updated: 2023-06-12 09:52:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/56308c9.html","posts/cc9b0903.html","posts/7aba474f.html","posts/5c21bb3.html","posts/cee7e62d.html","posts/7218365e.html","posts/df5af0f1.html","posts/e7e9fba0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/56308c9.html","posts/5c21bb3.html","posts/cc9b0903.html","posts/7218365e.html","posts/7aba474f.html","posts/cee7e62d.html","posts/e7e9fba0.html","posts/df5af0f1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
